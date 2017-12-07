4b37b8a6c25da20abb37f70c16e875e2fb8c0a46
Singh, Shivam D <shivam.d.singh@accenture.com>
2017-12-07 13:22:03 +0530

    closing in the avatar in scene21

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -399,7 +399,7 @@ export class HomePage {
                     child.material.shading = FlatShading;
                 }
             });
-            obj.rotation.x = 120;
+            obj.rotation.x = 75;
             // obj.rotation.y = 45;
             obj.position.z = 0.5;
             // obj.position.z = -7;
